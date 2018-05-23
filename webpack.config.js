edd6a5cb460c6f9eda22a645c1b31cb46a61b0e7
vlescot <vincent.lescot@gmail.com>
2018-05-23 15:44:45 +0200

    #2 Part of Twig Template (Used to share with mentor)

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-var Encore = require('@symfony/webpack-encore');
+let Encore = require("@symfony/webpack-encore");
 
 Encore
     // the project directory where all compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath("public/build/")
 
     // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
+    .setPublicPath("/build")
 
     // will create public/build/app.js and public/build/app.css
-    .addEntry('app', './public/assets/js/base.js')
+    .addEntry("app", "./public/assets/js/base.js")
 
     .enableBuildNotifications()
 ;
